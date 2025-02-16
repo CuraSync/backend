4cf1a2839213ae6700397c62383572ea484a270b
Neth Botheju <nethbotheju@gmail.com>
2025-02-16 22:02:07 +0530

    CPG-8 add middleware and models for authentication and data handling
    Implemented essential middleware for request validation, authentication, and error handling.
    Added database models to structure and manage application data.
    
    Co-Authored-By: Venul Minsara <188411232+menulVIN@users.noreply.github.com>

---
+++ src/middleware/authenticateToken.js
@@ -0,0 +1,11 @@
+const { verifyToken } = require("../config/jwtUtils");
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  verifyToken(token, req, res, next);
+};
+
+module.exports = authenticateToken;
