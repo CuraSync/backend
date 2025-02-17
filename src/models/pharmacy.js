00fba615f15e44b5777a232977208a31b1fb1851
Neth Botheju <nethbotheju@gmail.com>
2025-02-17 16:47:08 +0530

    CPG-13 created pharmacy model
    Created the pharmacy model with the finalized data scheme.
    
    Co-Authored-By: Venul Minsara <188411232+menulVIN@users.noreply.github.com>

---
+++ src/models/pharmacy.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const pharmacySchema = new mongoose.Schema(
+  {
+    pharmacyId: { type: String, required: true, unique: true, immutable: true },
+    pharmacyName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    licenceNumber: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String },
+    operatingHours: { type: String },
+    rating: { type: Number, min: 0, max: 5 },
+    profilePic: { type: String },
+    contactInformation: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Pharmacy", pharmacySchema, "pharmacy");
+
+module.exports = Doctor;
